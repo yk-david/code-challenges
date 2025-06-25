762d9b5acdb72eb2bf819fd1b8b9cff9600bc955
yk-david <k.yongkeon@gmail.com>
2025-06-26 00:01:28 +0900

    initialize challenge

---
+++ emoji-slot-machine/index.js
@@ -0,0 +1,25 @@
+/*
+   Let's create an emoji slot machine! Replace the hardcoded 
+   data with random fruit emojis from an emojis API. 
+   
+   - Request emoji food data from the API resource below. Log it and look at it!
+        - Write a function that takes in the data and returns a new array of only
+        fruit emoji objects
+        - Write a function to get 9 random fruits from your new array of fruit 
+        
+   - Load nine random fruits into the slot machine
+*/ 
+
+const slotMachine = document.querySelector('.emoji-slots-game'); 
+const food = 'https://apis.scrimba.com/emojihub/api/all/category/food-and-drink';
+
+function makeFruitArray(arr){
+   
+}
+
+function getRandomFruits(arr){
+
+}
+
+// write your fetch request here 
+console.log("hello!")
