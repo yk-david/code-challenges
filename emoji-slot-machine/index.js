963e7b3bac2014a39b92e5def605c884b7801301
yk-david <k.yongkeon@gmail.com>
2025-06-26 12:58:19 +0900

    made working solution on emoji-slot-machine

--- emoji-slot-machine/index.js
+++ emoji-slot-machine/index.js
@@ -8,32 +8,39 @@
         - Write a function to get 9 random fruits from your new array of fruit 
         
    - Load nine random fruits into the slot machine
-*/ 
+*/
 
-const slotMachine = document.querySelector('.emoji-slots-game'); 
-const url = 'https://apis.scrimba.com/emojihub/api/all/category/food-and-drink';
+const slotMachine = document.querySelector(".emoji-slots-game");
+const url = "https://apis.scrimba.com/emojihub/api/all/category/food-and-drink";
 
 async function makeFruitArray(callbackFn) {
-   return await callbackFn
-    .then(foodArray => foodArray.filter(food => food.group === "food fruit"));
+  return await callbackFn.then((foodArray) =>
+    foodArray.filter((food) => food.group === "food fruit")
+  );
 }
 
 async function getRandomFruits(callbackFn) {
   // How can I get 9 random numbers?
   const fruits = await makeFruitArray(getFruits())
-    .then(data => data)
-    .catch(error => console.log(`Something went wrong...: ${error}`));
-  
+    .then((data) => data)
+    .catch((error) => console.log(`Something went wrong...: ${error}`));
+
   const randomFruits = [];
 
-  while (randomFruits.length <= 9) {
+  while (randomFruits.length <= 8) {
     if (!randomFruits.includes(fruits[getRandomIndex(fruits)])) {
       randomFruits.push(fruits[getRandomIndex(fruits)]);
     }
+    console.log(`randomFruits has now ${randomFruits.length} fruits!`);
   }
+
+  randomFruits.forEach(
+    (fruit) => (slotMachine.innerHTML += `<li>${fruit.htmlCode}</li>`)
+  );
+  // return randomFruits;
 }
 
-// write your fetch request here 
+// write your fetch request here
 async function getFruits() {
   const response = await fetch(url);
   const data = await response.json();
@@ -45,12 +52,7 @@ function getRandomIndex(arr) {
   return Math.floor(Math.random() * arr.length);
 }
 
-// getFruits(url)
-//   .then(data => console.log(data))
-//   .catch(error => console.error(error))
-
-// makeFruitArray(getFruits())
-//   .then(data => console.log(data));
-
-getRandomFruits(makeFruitArray(getFruits()))
-  .then(data => console.log(data));
+// Getting randomly fruits icons and render
+const renderFruitsIcon = getRandomFruits(makeFruitArray(getFruits())).then(
+  (data) => data
+);
